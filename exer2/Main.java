fdbb6a784001ddc1f348e81c34c18fe5a7112884
Zed14Medina <zimedina@up.edu.ph>
2025-02-22 11:32:16 +0800

    Update Main.java

--- exer2/Main.java
+++ exer2/Main.java
@@ -1,14 +1,28 @@
+
 public class ListNode {
     int val;
     ListNode next;
     ListNode(int val) { this.val = val; this.next = null; }
 
+     public static ListNode deserialize(String data) {
+        if (data == null || data.equals("[]")) return null;
+        
+         String[] values = data.substring(1, data.length() - 1).split(",");
+        ListNode head = new ListNode(Integer.parseInt(values[0]));
+        ListNode current = head;
+
+        for (int i = 1; i < values.length; i++) {
+            current.next = new ListNode(Integer.parseInt(values[i]));
+            current = current.next;
+        }
+        return head;
+    }
 }
 
 class Solution {
     public ListNode deleteDuplicates(ListNode head) {
-        if (head == null) return null;
-
+         if (head == null) return null;
+        
         ListNode current = head;
         while (current !=null && current.next != null){
             if (current.val == current.next.val) {
